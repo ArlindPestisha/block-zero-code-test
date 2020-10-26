5a0716e095e7344ec30c7c84b9e0d33b4c25a800
Arlind Pestisha <arlind.pestisha@hyperisland.se>
2020-10-26 10:47:06 +0100

    starting to work in JS first targeting all the elements from HTML

---
+++ script.js
@@ -0,0 +1,5 @@
+//All the HTML elements
+const activePlayer = document.querySelector('.active-player')
+const resetGame = document.querySelector('.reset')
+const gameSquare = document.querySelectorAll('.game-square')
+console.log(gameSquare)
