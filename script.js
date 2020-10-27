34ec811a6354181fddee87cc3efa9b5b1fcee3e0
Arlind Pestisha <arlind.pestisha@hyperisland.se>
2020-10-27 14:48:06 +0100

    adding class whene we click in gameSquares with if else

--- script.js
+++ script.js
@@ -16,8 +16,23 @@ const handleReset = (e) => {
 }
 
 const handleSquareClick = (e) => {
-    console.log(e)
+    const classList = e.target.classList
+    const location = classList[1]
+    
+    if(classList[2] === 'x' || classList[2] === 'o'){
+        return
+    }
+    
+    if (xIsNext) {
+        classList.add('x')
+        xIsNext = !xIsNext
+    } else {
+        classList.add('o')
+        xIsNext= !xIsNext
+    }
 }
+
+
 
 
 //event listeners
